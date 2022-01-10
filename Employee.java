40daac6fb220299e8d62c40f55ce1dad801442c3
Shirinzoy <89048476+Shirinzoy@users.noreply.github.com>
2022-01-10 15:55:14 -0500

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,18 @@
+package Collections;
+	// 1. create class Employee
+public class Employee {
+	// 2. create some global variables
+	String name;
+	int age;
+	String dept;
+	
+	// 3. create method called Employee, and inside the method marameter pass the above variables
+	
+	Employee(String name, int age, String dept){
+	// 4. declare variables with this keyword
+		this.name = name;
+		this.age = age;
+		this.dept = dept;
+	// 5. go back to ArrayConcept class and follow step13.
+	}
+}
