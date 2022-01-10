4b892ca6d3fcbabe793016f9b832f71222bbefa1
Shirinzoy <rahim.shirinzoy@gmail.com>
2022-01-10 15:47:52 -0500

    second commit

--- src/myPackage/MyBrothersClass.java
+++ src/myPackage/MyBrothersClass.java
@@ -4,7 +4,10 @@ public class MyBrothersClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 39;
+		int b = 34;
+		int c = a+b;
+		System.out.println(c);
 	}
 
 }
